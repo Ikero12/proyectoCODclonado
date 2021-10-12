db3df7c0e62a8ee0650672583c5505187e6e80ab
Wolke <afernandezmartinez@danielcastelao.org>
2021-10-12 16:53:51 +0200

    "Hacemos el primer commit"

---
+++ src/com/DAM1/Paquete1/Clase1.java
@@ -0,0 +1,8 @@
+package com.DAM1.Paquete1;
+
+public class Clase1 {
+
+    public static void main(String[] args) {
+        System.out.println("Esto es la clase 1");
+    }
+}
---
+++ src/com/DAM1/Paquete2/Clase2.java
@@ -0,0 +1,7 @@
+package com.DAM1.Paquete2;
+
+public class Clase2 {
+    public static void main(String[] args) {
+        System.out.println("Esto es la clase 2");
+    }
+}
